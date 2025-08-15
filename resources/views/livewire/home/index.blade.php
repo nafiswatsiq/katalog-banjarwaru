f4cd8db0d555bd78d3c60c85e4cb9367f864053e
nafiswatsiq <amrullohnafis@gmail.com>
2025-08-15 11:23:57 +0700

    chore: install filament

---
+++ resources/views/livewire/home/index.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Tempek
+</div>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\Auth\Passwords\Email;
 use App\Livewire\Auth\Passwords\Reset;
 use App\Livewire\Auth\Register;
 use App\Livewire\Auth\Verify;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+// Route::view('/', 'welcome')->name('home');
+Route::get('/', Home\Index::class)->name('home');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
